9b2136ab8ec0ba2ceb316e4ec9091ad9993fe7a4
Hussain Nawaz Lalee <38792700+Lalee10@users.noreply.github.com>
2020-06-07 22:00:19 +0500

    Fix build issues

--- src/components/error/codes.ts
+++
@@ -1,8 +0,0 @@
-const errCodes = {
-	duplicateEntity: "DUPLICATE_ENTITY",
-	invalidInput: "INVALID_INPUT",
-	notAuthenticated: "UNAUTHORIZED",
-	invalidCredentials: "AUTH_FAILED",
-}
-
-export default errCodes
